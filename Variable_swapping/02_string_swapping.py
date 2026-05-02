69b4e1dbc5d317240c63143d4198590133e07d23
sudhirkushwaha10-hue <sudhirkushwaha10@gmail.com>
2026-05-02 23:05:05 +0530

    Variable_swapping

---
+++ Variable_swapping/02_string_swapping.py
@@ -0,0 +1,11 @@
+chart1="hello"
+char2="java"
+print(f"Before Swapping chart1: {"hello"}")
+print(f"Before Swapping chart2: {"java"}")
+
+a= "chart1"
+chart1= "chart2"
+chart2= "a"
+
+print(f"After Swapping chart1:{"chart1"}")
+print(f"After Swapping chart2:{"chart2"}")
---
+++ Variable_swapping/03_string_to_Integer.py
@@ -0,0 +1,5 @@
+n1=100
+chart2="java"
+print(f"Before swapping n1: {100}" )
+print(f"Befor swapping chart:{"java"}")
+print()
