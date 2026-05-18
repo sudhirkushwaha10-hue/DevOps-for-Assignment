46504ceb1aeea87ffcc32d0ea15a9d742088d585
sudhirkushwaha10-hue <sudhirkushwaha10@gmail.com>
2026-05-18 22:11:25 +0530

    update

---
+++ Assignment2/Assignment2/Task17.py
@@ -0,0 +1,9 @@
+a=int(input("num1:"))
+b=int(input("num2:"))
+c=int(input("num3:"))
+if a>=b and a>=c:
+    print("Greatest number is a:")
+elif b>=a and b>=c:
+    print("Greatest number is b:")
+elif c>=a and c>=b:
+    print("Greatest number is c:")
---
+++ Assignment2/Assignment2/Task18.py
@@ -0,0 +1,6 @@
+user_name=input("username:")
+password=input("password:")
+if user_name== user1 and password== Pass@123:
+    print("Authentication successful")
+else:
+    print("Authentication failed")
