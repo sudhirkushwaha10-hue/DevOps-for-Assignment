ce6d31f37e8e5bdf8233e7e9d67545d0fac8b993
sudhirkushwaha10-hue <sudhirkushwaha10@gmail.com>
2026-05-18 22:15:57 +0530

    update

--- Assignment2/Assignment2/Task18.py
+++ Assignment2/Assignment2/Task18.py
@@ -1,6 +1,2 @@
 user_name=input("username:")
 password=input("password:")
-if user_name== user1 and password== Pass@123:
-    print("Authentication successful")
-else:
-    print("Authentication failed")
